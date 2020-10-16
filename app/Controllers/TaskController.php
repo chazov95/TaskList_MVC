83c7a223f4f048abacaa8d74ad8140e9917929dd
Alexander Chazov <chazov.a95@gmail.com>
2020-10-16 17:55:01 +0400

    start project

---
+++ app/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+class taskController
+{
+    public function addTask(){
+
+    }
+    public function delTask(){
+
+    }
+    public function changeTaskStatus(){
+
+    }
+    public function delAllTask(){
+
+    }
+}
---
+++ app/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+class userController
+{
+    public function addUser(){
+
+    }
+    public function login(){
+
+    }
+
+    public function logOut(){
+
+    }
+}
